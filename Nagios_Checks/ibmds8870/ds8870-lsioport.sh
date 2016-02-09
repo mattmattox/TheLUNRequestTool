c0deac4f2914b2b5b5245dee4a46967a033a9aa1
mattmattox <matt@mattox.work>
2016-02-09 11:27:51 -0600

    Importing more DS8870 Nagios Checks

---
+++ Nagios_Checks/ibmds8870/ds8870-lsioport.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+########################################################################################
+#
+#       Filename: ds8870-lsioport.sh
+#       Author: Matthew Mattox matt@mattox.work
+#       Date: 02/03/2015
+#       Purpose: Nagios Service Check to verify IO Port status
+#       Update: Updated to include the CFG files.
+#
+########################################################################################
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+Nagios Service Check to verify disk array status.
+
+OPTIONS:
+   -h      Show this message
+   -S      ds8870 Name     			Example: chidc8870
+   -D      Device ID                            Example: IBM.2107-75BMP51
+EOF
+}
+
+while getopts .h:S:D:V.v. OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         S)
+             DS8870NAME=$OPTARG
+             ;;
+         D)
+             DEVID=$OPTARG
+             ;;
+         V)
+             VERBOSE=1
+             ;;
+         v)
+             VERBOSE=1
+             ;;
+         ?)
+            usage
+             exit
+             ;;
+     esac
+done
+if [[ -z $DS8870NAME ]]
+then
+        usage
+        exit 1
+fi
+
+##Overview - start
+if [ "$VERBOSE" = "1" ];
+then
+echo "########################################"
+echo "DS8870 Name:" $DS8870NAME
+echo "Dev ID:" $DEVID
+echo "########################################"
+read -p "Please press [Enter] to continue..."
+fi
+##Overview - end
+if [ "$DS8870NAME" == "a1ds88701" ] || [ "$DS8870NAME" == "A1DS88701" ]
+then
+	DS8870NAME="A1DS88701"
+	CFG="/opt/ibm/dscli/profile/A1DS88701.profile"
+fi
+if [ "$DS8870NAME" == "b1ds88701" ] || [ "$DS8870NAME" == "B1DS88701" ]
+then
+	DS8870NAME="B1DS88701"
+        CFG="/opt/ibm/dscli/profile/B1DS88701.profile"
+fi
+if [ "$DS8870NAME" == "c1ds88701" ] || [ "$DS8870NAME" == "C1DS88701" ]
+then
+	DS8870NAME="C1DS88701"
+    CFG="/opt/ibm/dscli/profile/C1DS88701.profile"
+fi
+
+DEVID=$(cat $CFG | grep ^devid | awk '{print $2}')
+
+
+if [ "$VERBOSE" = "1" ];
+then
+	echo "##############################"
+	echo "RAW DATA"
+	/opt/ibm/dscli/dscli -cfg $CFG "lsioport -fmt delim -hdr off -dev $DEVID"
+	echo "##############################"
+fi
+####Get array status - start
+STATUS=$(/opt/ibm/dscli/dscli -cfg $CFG "lsioport -fmt delim -hdr off -dev $DEVID" | grep -v "Online")
+####Get array status - end
+
+if [[ -z "$STATUS" ]];
+then
+	echo "OK: All IO Ports are Online"
+        exit 0
+else
+	echo "CRITICAL: Problem found with IO Port" $(echo $STATUS | grep -v "Online" | awk -F"," '{print $1}')
+        exit 2
+fi
---
+++ Nagios_Checks/ibmds8870/ds8870-lsserver.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+########################################################################################
+#
+#       Filename: ds8870-lsserver.sh
+#       Author: Matthew Mattox matt@mattox.work
+#       Date: 12/18/2014
+#       Purpose: Nagios Service Check to verify disk array status
+#       Update: Updated to include the CFG files.
+#
+########################################################################################
+
+usage()
+{
+cat << EOF
+usage: $0 options
+
+Nagios Service Check to verify disk array status.
+
+OPTIONS:
+   -h      Show this message
+   -S      ds8870 Name     			Example: chidc8870
+EOF
+}
+
+while getopts .h:S:A:V.v. OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         S)
+             DS8870NAME=$OPTARG
+             ;;
+         V)
+             VERBOSE=1
+             ;;
+         v)
+             VERBOSE=1
+             ;;
+         ?)
+            usage
+             exit
+             ;;
+     esac
+done
+if [[ -z $DS8870NAME ]]
+then
+        usage
+        exit 1
+fi
+
+##Overview - start
+if [ "$VERBOSE" = "1" ];
+then
+echo "########################################"
+echo "DS8870 Name:" $DS8870NAME
+echo "########################################"
+read -p "Please press [Enter] to continue..."
+fi
+##Overview - end
+CFG=""
+
+if [ "$DS8870NAME" == "a1ds88701" ] || [ "$DS8870NAME" == "A1DS88701" ]
+then
+	DS8870NAME="A1DS88701"
+	CFG="/opt/ibm/dscli/profile/A1DS88701.profile"
+fi
+if [ "$DS8870NAME" == "b1ds88701" ] || [ "$DS8870NAME" == "B1DS88701" ]
+then
+	DS8870NAME="B1DS88701"
+        CFG="/opt/ibm/dscli/profile/B1DS88701.profile"
+fi
+if [ "$DS8870NAME" == "c1ds88701" ] || [ "$DS8870NAME" == "C1DS88701" ]
+then
+	DS8870NAME="C1DS88701"
+    CFG="/opt/ibm/dscli/profile/C1DS88701.profile"
+fi
+
+DEVID=$(cat $CFG | grep ^devid | awk '{print $2}')
+
+if [ "$VERBOSE" = "1" ];
+then
+	echo "##############################"
+	echo "RAW DATA"
+	/opt/ibm/dscli/dscli -cfg $CFG "lsserver -fmt delim -hdr off"
+	echo "##############################"
+fi
+####Get array status - start
+STATUS=$(/opt/ibm/dscli/dscli -cfg $CFG "lsserver -fmt delim -hdr off" | grep -v ",online")
+####Get array status - end
+
+if [[ -z "$STATUS" ]];
+then
+        echo "OK: All Servers are Normal"
+        exit 0
+else
+        echo "CRITICAL: Problem found with Server" $(echo $STATUS | grep -v ",online" | awk -F"," '{print $1}')
+        exit 2
+fi
